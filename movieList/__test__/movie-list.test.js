463f6e3d3e95203bace531965c45ab339b05fd93
Cole Callaway <nccallaway54@gmail.com>
2022-04-30 19:33:50 -0600

    add movie test

--- movieList/__test__/movie-list.test.js
+++ movieList/__test__/movie-list.test.js
@@ -1,13 +1,19 @@
-const { Builder, Capabilities, By } = require("selenium-webdriver");
+const { Builder, Capabilities } = require("selenium-webdriver");
 require("chromedriver");
 
-const driver = new Builder().withCapabilities(Capabilities.chrome());
+const driver = new Builder().withCapabilities(Capabilities.chrome()).build();
 
-beforeAll(async () => {
+const { addMovie } = require("../functions/functions");
+
+beforeEach(async () => {
   await driver.get("http://127.0.0.1:5500/movieList/index.html");
 });
 
 afterAll(async () => {
-  await driver.sleep(1000);
-  await driver.quit();
+  //   await driver.sleep(1000);
+  //   await driver.quit();
+});
+
+it("should add a new movie to list", async () => {
+  await addMovie(driver);
 });
---
+++ movieList/functions/functions.js
@@ -0,0 +1,21 @@
+const { By } = require("selenium-webdriver");
+
+const addMovie = async (driver) => {
+  const addMovieInput = await driver.findElement(By.xpath("//input"));
+
+  addMovieInput.sendKeys("Indiana Jones");
+
+  const addMovieBtn = await driver.findElement(By.xpath("//button"));
+
+  addMovieBtn.click();
+
+  await driver.sleep(1000);
+
+  const movie = await driver.findElement(By.xpath("//li"));
+
+  const isDisplayed = movie.isDisplayed();
+
+  expect(isDisplayed).toBeTruthy();
+};
+
+module.exports = { addMovie };
